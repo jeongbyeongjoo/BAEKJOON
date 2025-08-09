77b47f7315115f4d8a218a4fa456ad15b9e1a669
JeongByeongJoo <jbj030108@icloud.com>
2025-08-09 14:08:06 +0900

    update

--- practice.py
+++ practice.py
@@ -1,13 +1,21 @@
-N, K = map(int, input().split())
-count = 0
 arr = []
 
-for i in range(1, N + 1):
-    if N % i == 0:
-        count += 1
-    if count == K:
-        print(i)
-        exit()
+while(True):
+    n = int(input())
+    
+    if n == -1:
+        break
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(str(n) + " = ", end="")
+        for i in range(len(arr) - 1):
+            print(arr[i], end="")
+            print(" + ", end="")
+        print(arr[-1])
+    else:
+        print(str(n) + " is NOT perfect.")
+    arr.clear()
         
-print(0)
     
