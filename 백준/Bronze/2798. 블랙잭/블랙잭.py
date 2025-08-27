7e4eff0a7525ed1ec266990d8314b599a327e719
jeongbyeongjoo <103037489+jeongbyeongjoo@users.noreply.github.com>
2025-08-27 21:51:33 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+
+result = 0
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for k in range(j+1, N):
+            sum = arr[i] + arr[j] + arr[k]
+            if (M - sum < abs(M - result) and M-sum >= 0):
+                result = sum
+                
+print(result)
