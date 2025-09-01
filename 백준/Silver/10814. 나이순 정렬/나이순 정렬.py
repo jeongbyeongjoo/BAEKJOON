38aff6ee9d076ae56339cccc28e6e076d00d9064
jeongbyeongjoo <103037489+jeongbyeongjoo@users.noreply.github.com>
2025-09-01 14:08:19 +0900

    [Silver V] Title: 나이순 정렬, Time: 264 ms, Memory: 64212 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,12 +1,13 @@
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 
 arr = []
 for i in range(N):
-    input_data = input().split()
+    input_data = sys.stdin.readline().split()
     input_data.append(i)
     arr.append(input_data)
     
 arr_sorted = sorted(arr, key=lambda p:(int(p[0]), p[2]))
 
 for element in arr_sorted:
-    print(element[0], element[1])
+    sys.stdout.write(element[0] + " " + element[1] + "\n")
