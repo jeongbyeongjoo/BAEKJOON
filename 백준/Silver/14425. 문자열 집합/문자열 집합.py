b2de0fda84cf0262faa26763c13f93d376a8d4b0
jeongbyeongjoo <103037489+jeongbyeongjoo@users.noreply.github.com>
2025-09-02 19:16:56 +0900

    [Silver IV] Title: 문자열 집합, Time: 92 ms, Memory: 37432 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,15 +1,14 @@
-N, M = map(int, input().split())
-1
-set_N = set()
-set_M = set()
-for _ in range(N):
-    set_N.add(input())
+import sys
+input = sys.stdin.readline
 
+N, M = map(int, input().split())
+
+S = set(input().strip() for _ in range(N))  # 문자열 집합
 count = 0
+
 for _ in range(M):
-    input_data = input()
-    if input_data in set_N:
-        count+=1
-      
+    word = input().strip()
+    if word in S:
+        count += 1
+
 print(count)
-    
