47c19336e6a186f12f18d116c16c13731001e9fa
JEONG BYEONGJOO <103037489+jeongbyeongjoo@users.noreply.github.com>
2026-06-22 00:49:18 +0900

    [Passed] 각 정점까지의 최단 경로 2 (208ms, 27MB)

---
+++ 코드트리/trail5/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+INT_MAX = float('inf')
+dist = [[INT_MAX]*(n+1) for _ in range(n+1)]
+
+for u, v, w in edges:
+    dist[u][v] = min(dist[u][v], w)
+
+for i in range(1, n+1):
+    dist[i][i] = 0
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if dist[i][j] == INT_MAX:
+            print(-1, end=" ")
+        else:
+            print(dist[i][j], end=" ")
+    print()
