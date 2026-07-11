abefd354ad81c2f1985096eb679e347203efaceb
JEONG BYEONGJOO <103037489+jeongbyeongjoo@users.noreply.github.com>
2026-07-11 14:50:01 +0900

    [Passed] 괄호 쌍 만들어주기 2 (50ms, 16MB)

---
+++ 코드트리/trail2/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,12 @@
+# 5분 14초
+
+A = input()
+
+cnt = 0
+
+for i in range(len(A)-3):
+    for j in range(i+2, len(A)-1):
+        if A[i] == '(' and A[i+1] == '(' and A[j] == ')' and A[j+1] == ')':
+            cnt += 1
+
+print(cnt)
