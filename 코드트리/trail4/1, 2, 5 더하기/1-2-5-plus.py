d1179a9c34fc12a6eb14687e741b2390d96c6e78
JEONG BYEONGJOO <103037489+jeongbyeongjoo@users.noreply.github.com>
2026-08-19 10:02:48 +0900

    [Passed] 1, 2, 5 더하기 (47ms, 16MB)

---
+++ 코드트리/trail4/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,16 @@
+# 7분 30초
+
+n = int(input())
+
+dp = [0]*(1001)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 3
+dp[4] = 5
+dp[5] = 9
+
+for i in range(6, n+1):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-5]
+
+print(dp[n]%10007)
