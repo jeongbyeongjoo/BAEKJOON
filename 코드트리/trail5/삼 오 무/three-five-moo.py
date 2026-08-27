9ecc27ac567f3256597fef92cd8cd5ee072ed484
JEONG BYEONGJOO <103037489+jeongbyeongjoo@users.noreply.github.com>
2026-08-27 14:42:36 +0900

    [Passed] 삼 오 무 (48ms, 16MB)

--- 코드트리/trail5/삼 오 무/three-five-moo.py
+++ 코드트리/trail5/삼 오 무/three-five-moo.py
@@ -1,27 +1,19 @@
-# 못품 (그냥 여기 문제는 시험 때 패스하닌게 나을지도...)
-# 다시 풀기(삼오무로 푸는 이 로직을 기억해야함)
-# is_possible()에 리턴값이랑 n은 입력부분(4)으로 생각해서 서로 비교하게 냅두고
-# 실제로 구한는 위치값?을 출력(while문의 left, right로 나오는 숫자)으로 설정(7)
-# INT_MAX = float('inf')로 풀면 오류남
+# 8분 32초
+
 n = int(input())
 
-def solve(num):
-    sum = num
-    sum -= num // 3
-    sum -= num // 5
-    sum += num // 15
+def solve(mid):
+    return mid - mid//3 - mid//5 + mid//15
 
-    return n <= sum
-
-left = 1
-right = 1000000000000
-mid_num = 1000000000000
+left = 0
+right = 10**18
+mid_num = 10**18
 
 while left <= right:
     mid = (left + right) // 2
-    if solve(mid):
-        right = mid - 1
+    if solve(mid) >= n:
         mid_num = min(mid_num, mid)
+        right = mid -1 
     else:
         left = mid + 1
 
