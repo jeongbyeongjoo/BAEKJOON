2e5692816ec52430a20ea5f4a3e9c40786775186
JEONG BYEONGJOO <103037489+jeongbyeongjoo@users.noreply.github.com>
2026-09-04 15:59:20 +0900

    [Passed] 마을 구분하기 (49ms, 16MB)

--- 코드트리/trail4/마을 구분하기/seperate-village.py
+++ 코드트리/trail4/마을 구분하기/seperate-village.py
@@ -1,13 +1,15 @@
+# 27분 30초
+
+import sys
+
+sys.setrecursionlimit(1000000)
+
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-
-visited = [[0]*n for _ in range(n)]
-arr = []
-cnt = 0
-order = 1
+visited = [[False]*n for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    return 0 <= x and x < n and 0 <= y and y < n
 
 def can_go(x, y):
     if not in_range(x, y):
@@ -16,31 +18,36 @@ def can_go(x, y):
         return False
     return True
 
-def dfs(x, y):
-    global order
+human_cnt = 0
 
-    dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
+def dfs(x, y):
+    global human_cnt
+
+    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+    visited[x][y] = True
+    human_cnt += 1
 
     for dx, dy in zip(dxs, dys):
         new_x = x + dx
         new_y = y + dy
+
         if can_go(new_x, new_y):
-            order += 1
-            visited[new_x][new_y] = 1
             dfs(new_x, new_y)
+
+village_cnt = 0
+arr = []
 
 for i in range(n):
     for j in range(n):
-        if grid[i][j] == 1:
-            if not visited[i][j]:
-                visited[i][j] = 1
-                dfs(i, j)
-                cnt += 1
-                arr.append(order)
-                order = 1
-
-
-print(cnt)
+        if not visited[i][j] and grid[i][j] == 1:
+            human_cnt = 0
+            dfs(i, j)
+            village_cnt += 1
+            arr.append(human_cnt)
+ 
+print(village_cnt)
 arr.sort()
+
 for elem in arr:
     print(elem)
